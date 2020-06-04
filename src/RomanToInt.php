30f011b735c6e5ac03a8a16b4a48eeb8d4bb28ff
jreuschenbach <j.reuschenbach@onOffice.com>
2020-06-04 21:28:33 +0200

    step 11-20  * nothing to do

--- src/RomanToInt.php
+++ src/RomanToInt.php
@@ -6,6 +6,16 @@ class RomanToInt
 {
     private $romanParts =
         [
+            20 => 'XX',
+            19 => 'XIX',
+            18 => 'XVIII',
+            17 => 'XVII',
+            16 => 'XVI',
+            15 => 'XV',
+            14 => 'XIV',
+            13 => 'XIII',
+            12 => 'XII',
+            11 => 'XI',
             10 => 'X',
             9 => 'IX',
             5 => 'V',
