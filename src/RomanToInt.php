7e6764389c3a070cbfe5b39e07b3c59a4d4d606b
jreuschenbach <j.reuschenbach@onOffice.com>
2020-06-04 21:12:48 +0200

    step 4: IV
    * fake it: if-check for 4

--- src/RomanToInt.php
+++ src/RomanToInt.php
@@ -6,6 +6,11 @@ class RomanToInt
 {
     public function convert($int)
     {
+        if ($int == 4)
+        {
+            return 'IV';
+        }
+
         return str_repeat('I', $int);
     }
 }
--- tests/RomanToIntTest.php
+++ tests/RomanToIntTest.php
@@ -9,6 +9,7 @@ class RomanToIntTest extends TestCase
         1 => 'I',
         2 => 'II',
         3 => 'III',
+        4 => 'IV',
     ];
 
     public function testConvert()
